78d93c3907dd831b72c79eaa447bbc647f4b35f9
AJSeidner <AJSeidner@gmail.com>
2017-10-06 19:06:57 -0500

    jawsdb update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 
-
+//mysql://w0u1pw93zmkbt2gn:xh3165qlnpj0mm9t@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/k8vpcodnfn9a58nm
 //for heroku deployment
 if(process.env.JAWSDB_URL){
     connection=mysql.createConnection(process.env.JAWSDB_URL);
@@ -9,7 +9,7 @@ else{
     connection=mysql.createConnection({
     host:"localhost",
     user: "root",
-    password:"Slipknot@14",
+    password:"",
     database: "inventory_db"
     })
 
